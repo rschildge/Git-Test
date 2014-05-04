d3428948d7d65f502238fa7685050705dff9f1e4
Ronald Schildge <rschildge@gmail.com>
2014-05-03 20:35:12 -0400

    first commit

---
+++ Git Test/ASPtest.h
@@ -0,0 +1,13 @@
+//
+//  ASPtest.h
+//  Git Test
+//
+//  Created by Ronald Schildge on 5/3/14.
+//  Copyright (c) 2014 Ronald Schildge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ASPtest : NSObject
+
+@end
---
+++ Git Test/ASPtest.m
@@ -0,0 +1,13 @@
+//
+//  ASPtest.m
+//  Git Test
+//
+//  Created by Ronald Schildge on 5/3/14.
+//  Copyright (c) 2014 Ronald Schildge. All rights reserved.
+//
+
+#import "ASPtest.h"
+
+@implementation ASPtest
+
+@end
